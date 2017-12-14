6137ea793e96533c8664e396c4d3602c1addba66
devsadventure <margaret.the.adventurer@gmail.com>
2017-12-14 17:40:38 +0900

    뱀 위치 변경

--- src/SnakeGameLogic.js
+++ src/SnakeGameLogic.js
@@ -1,49 +1,100 @@
-import {ROWS, COLS} from './config';
+import {
+  ROWS,
+  COLS
+} from './config';
 
 function SnakeGameLogic() {
   // 각 마디의 좌표를 저장하는 배열
-  this.joints = [{x: 2, y: 0}, {x: 1, y: 0}, {x: 0, y: 0}];
+  this.joints = [{
+      x: 3,
+      y: 0
+    },
+    {
+      x: 2,
+      y: 0
+    },
+    {
+      x: 1,
+      y: 0
+    },
+    {
+      x: 0,
+      y: 0
+    }
+  ];
   // 먹이의 좌표
-  this.fruit = {x: 5, y: 10};
+  this.fruit = {
+    x: 5,
+    y: 10
+  };
+  this.direction = "right";
 }
 
-SnakeGameLogic.prototype.up = function() {
+
+SnakeGameLogic.prototype.up = function () {
+  this.joints.splice(0, 1, {
+    x: this.joints[0].x - 1,
+    y: this.joints[0].y - 1
+  });
+  this.direction = "up";
   console.log('up');
-  for(let i of this.joints){
-    i.y--;
-  }
 }
 
-SnakeGameLogic.prototype.down = function() {
+SnakeGameLogic.prototype.down = function () {
+  this.joints.splice(0, 1, {
+    x: this.joints[0].x - 1,
+    y: this.joints[0].y
+  });
+  this.direction = "down";
   console.log('down');
-  for (let i of this.joints) {
-    i.y++;
-  }
 }
 
-SnakeGameLogic.prototype.left = function() {
+SnakeGameLogic.prototype.left = function () {
+  this.joints.splice(0, 1, {
+    x: this.joints[0].x - 1,
+    y: this.joints[0].y - 1
+  });
+  this.direction = "left";
   console.log('left');
-  for (let i of this.joints) {
-    i.x--;
-  }
 }
 
-SnakeGameLogic.prototype.right = function() {
+SnakeGameLogic.prototype.right = function () {
+  this.joints.splice(0, 1, {
+    x: this.joints[0].x,
+    y: this.joints[0].y - 1
+  });
+  this.direction = "right";
   console.log('right');
-  for (let i of this.joints) {
-    i.x++;
-  }
 }
 
-SnakeGameLogic.prototype.nextState = function() {
-    // 한 번 움직여야 할 타이밍마다 실행되는 함수
-    // 게임이 아직 끝나지 않았으면 `true`를 반환
-    // 게임이 끝났으면 `false`를 반환
-    console.log(`nextState`);
-  for (let i of this.joints) {
-    i.x++;
+SnakeGameLogic.prototype.nextState = function () {
+  // 한 번 움직여야 할 타이밍마다 실행되는 함수
+  // 게임이 아직 끝나지 않았으면 `true`를 반환
+  // 게임이 끝났으면 `false`를 반환
+  console.log(`nextState`);
+  this.joints.pop();
+  if (this.direction === 'right') {
+    this.joints.unshift({
+      x: this.joints[0].x + 1,
+      y: this.joints[0].y
+    });
+  } else if (this.direction === 'left') {
+    this.joints.unshift({
+      x: this.joints[0].x - 1,
+      y: this.joints[0].y
+    });
+  } else if (this.direction === 'down') {
+    this.joints.unshift({
+      x: this.joints[0].x,
+      y: this.joints[0].y + 1
+    });
+  } else if (this.direction === 'up') {
+    this.joints.unshift({
+      x: this.joints[0].x,
+      y: this.joints[0].y - 1
+    });
   }
-    return true;
+  return true;
 }
 
 export default SnakeGameLogic;
