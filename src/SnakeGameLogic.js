bc20e194d4a618139eb30af97be7bfa9f9874531
devsadventure <margaret.the.adventurer@gmail.com>
2017-12-15 16:26:47 +0900

    초기화

--- src/SnakeGameLogic.js
+++ src/SnakeGameLogic.js
@@ -5,61 +5,31 @@ import {
 
 function SnakeGameLogic() {
   // 각 마디의 좌표를 저장하는 배열
-  this.joints = [{
-      x: 3,
-      y: 0
-    },
-    {
-      x: 2,
-      y: 0
-    },
-    {
-      x: 1,
-      y: 0
-    },
-    {
-      x: 0,
-      y: 0
-    }
+  this.joints = [
+    {x: 2, y: 0},
+    {x: 1, y: 0},
+    {x: 0, y: 0}
   ];
   // 먹이의 좌표
   this.fruit = {
-    x: Math.floor(Math.random() * 19),
-    y: Math.floor(Math.random() * 15)
+    x: 3,
+    y: 5
   };
-  this.direction = "right";
-  this.condition = "not_eaten";
-  this.head = this.joints[0];
-  this.newHead;
 }
 
-this.fruit
-
 SnakeGameLogic.prototype.up = function () {
-  if(this.direction!=='down'){
-    this.direction = "up";
-  }
   console.log('up');
 }
 
 SnakeGameLogic.prototype.down = function () {
-  if(this.direction !== 'up'){
-    this.direction = "down";
-  }
   console.log('down');
 }
 
 SnakeGameLogic.prototype.left = function () {
-  if(this.direction!=='right'){
-    this.direction = "left";
-  }
   console.log('left');
 }
 
 SnakeGameLogic.prototype.right = function () {
-  if(this.direction!=='left'){
-    this.direction = "right";
-  }
   console.log('right');
 }
 
@@ -68,44 +38,7 @@ SnakeGameLogic.prototype.nextState = function () {
   // 게임이 아직 끝나지 않았으면 `true`를 반환
   // 게임이 끝났으면 `false`를 반환
   console.log(`nextState`);
-  if (this.joints[0].x >= 19 || this.joints[0].y >= 15 || this.joints[0].x < 0 || this.joints[0].y < 0){
-    return false;
-  } else if (true) {
-    if (this.joints[0].x === this.fruit.x && this.joints[0].y === this.fruit.y) {
-      this.joints.push(this.fruit);
-      this.condition = 'is_eaten';
-    } else if (this.condition === 'is_eaten') {
-      this.fruit = {
-        x: Math.floor(Math.random() * 19),
-        y: Math.floor(Math.random() * 15)
-      };
-      this.condition = "not_eaten";
-    }
-    this.joints.pop();
-    if (this.direction === 'right') {
-      this.joints.unshift({
-        x: this.joints[0].x + 1,
-        y: this.joints[0].y
-      });
-    } else if (this.direction === 'left') {
-      this.joints.unshift({
-        x: this.joints[0].x - 1,
-        y: this.joints[0].y
-      });
-    } else if (this.direction === 'down') {
-      this.joints.unshift({
-        x: this.joints[0].x,
-        y: this.joints[0].y + 1
-      });
-    } else if (this.direction === 'up') {
-      this.joints.unshift({
-        x: this.joints[0].x,
-        y: this.joints[0].y - 1
-      });
-    }
-    console.log(this.joints);
-    return true;
-  }
+  return true;
 }
 
 export default SnakeGameLogic;
